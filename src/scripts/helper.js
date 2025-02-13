ca2a9ba9d5446f8681e65cd52f8a4cf5cdb25b08
raccoondays <43695821+raccoondays@users.noreply.github.com>
2025-02-13 04:08:19 +0200

    Fixed math

--- src/scripts/helper.js
+++ src/scripts/helper.js
@@ -5,10 +5,14 @@ class Helper {
 	getMousePos(canvas, evt, rect) {
 		return { x: evt.clientX - rect.left, y: evt.clientY - rect.top };
 	}
-	scaleNumber(num, oldRange, newRange){
-        let a = oldRange[0], b = oldRange[1], c = newRange[0], d = newRange[1];
-        return (b*c - (a)*d)/(b-a) + (num)*(d/(b-a));
-    }
+	scaleNumber(num, oldRange, newRange) {
+		const [oldMin, oldMax] = oldRange;
+		const [newMin, newMax] = newRange;
+
+		const proportion = (num - oldMin) / (oldMax - oldMin);
+
+		return newMin + proportion * (newMax - newMin);
+	}
     getIMG(blob, callback){
 		let a = new FileReader();
         a.onload = function(e) {
